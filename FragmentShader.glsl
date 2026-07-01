312622609395faabab6d95b1f76eb87679aac5a8
amgedtamer54321-droid <amgedtamer54321@gmail.com>
2026-07-01 22:11:39 +0300

    Update FragmentShader.glsl

--- FragmentShader.glsl
+++ FragmentShader.glsl
@@ -1,16 +1,11 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 FColor;
 in vec2 FTexture;
 
-
-
-uniform float color;
 uniform sampler2D texture1;
-uniform sampler2D texture2;
 
 void main()
 {
-	FragColor = mix(texture(texture1,FTexture),texture(texture2,FTexture),0.5);
+	FragColor = texture(texture1,FTexture);
 }
