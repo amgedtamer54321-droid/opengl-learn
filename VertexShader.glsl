e6b97f9b9ce293bb980399ce0a5ad93662ba7420
amgedtamer54321-droid <amgedtamer54321@gmail.com>
2026-07-01 22:11:25 +0300

    Update VertexShader.glsl

--- VertexShader.glsl
+++ VertexShader.glsl
@@ -1,7 +1,12 @@
 #version 330 core 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTexture;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 pre;
+
+
+
 
 out vec3 FColor;
 out vec2 FTexture;
@@ -10,10 +15,8 @@ out vec2 FTexture;
 void main()
 	{
 
-gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+gl_Position = pre * view * model * vec4(aPos, 1.0);
 
-
-FColor = aColor;
 FTexture = aTexture;
 
 }
