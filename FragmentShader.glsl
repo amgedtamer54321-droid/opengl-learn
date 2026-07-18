7740b670fa38e323c95c5d6863f48348063a34c4
amgedtamer54321-droid <amgedtamer54321@gmail.com>
2026-07-19 02:09:52 +0300

    Update FragmentShader.glsl

--- FragmentShader.glsl
+++ FragmentShader.glsl
@@ -1,11 +1,53 @@
 #version 330 core
+
 out vec4 FragColor;
+in  vec3 FragPos;
 
-in vec2 FTexture;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform mat3 Nmat;
+uniform vec3 camPos;
+float specularStrength = 0.5;
 
-uniform sampler2D texture1;
+struct Material {
+vec3 ambient;
+vec3 diffuse;
+vec3 specular;
+float shininess;
+};
 
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
+
+in vec3 oNormal;
 void main()
 {
-	FragColor = texture(texture1,FTexture);
+	vec3 Normal = Nmat * normalize(oNormal);
+	vec3 lightDir = normalize(light.position - FragPos);
+	vec3 norm = normalize(Normal);
+	vec3 viewDir = normalize(camPos - FragPos);
+
+   	vec3 ambient = light.ambient * material.ambient;
+	
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = light.diffuse * (diff * material.diffuse);
+
+
+	// specular
+	vec3 reflectDir = reflect(-lightDir, norm);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0),
+	material.shininess);
+	vec3 specular = light.specular * (spec * material.specular);
+
+
+
+	vec3 result = ambient + diffuse + specular;
+	FragColor = vec4(result, 1.0);
 }
